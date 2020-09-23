007108769537e5cdaf6cd827bae5198135905b3f
YuvalToker <yuval@gmail.com>
2020-09-23 14:56:16 +0300

    first try of snmpd with subagent

---
+++ dock_snmpd/instructions.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+echo "snmpd:127.0.0.1" >> /etc/hosts.allow
+echo "master agentx" >> /etc/snmp/snmpd.conf
+
+snmpd -f -Lf snmpd_file.log -C -M /usr/share/snmp/mibs --rwcommunity=public --master=agentx --agentXSocket=tcp:localhost:161 udp:1662
+./mysubagent -f -Lf subagent_file.log -x  tcp:localhost:161
--- dock_test/instructions.sh
+++ dock_test/instructions.sh
@@ -1,9 +1,2 @@
 #!/bin/bash
 
-declare -i x=1
-while true; do
-    sleep 5
-    snmptrap -v2c -c public testing_snmptrapd:162 '' SNMPv2-MIB::coldStart.0 SNMPv2-MIB::sysContact.0 s 'trap number #'$x
-    x=x+1
-done
-
