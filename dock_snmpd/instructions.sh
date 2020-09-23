bcacde38cc8406810adddfa96eb3a413ebb73248
YuvalToker <yuval@gmail.com>
2020-09-23 16:13:13 +0300

    writing snmp get & set command to file.log is now working

--- dock_snmpd/instructions.sh
+++ dock_snmpd/instructions.sh
@@ -4,5 +4,9 @@
 echo "snmpd:127.0.0.1" >> /etc/hosts.allow
 echo "master agentx" >> /etc/snmp/snmpd.conf
 
-snmpd -f -Lf snmpd_file.log -C -M /usr/share/snmp/mibs --rwcommunity=public --master=agentx --agentXSocket=tcp:localhost:161 udp:1662
-./mysubagent -f -Lf subagent_file.log -x  tcp:localhost:161
+snmpd -f -Lf snmpd_file.log -C -M /usr/share/snmp/mibs --rwcommunity=public --master=agentx --agentXSocket=tcp:localhost:161 udp:1662 &
+./mysubagent -f -Lf subagent_file.log -x  tcp:localhost:161 &
+
+while true; do
+    sleep 5
+done
