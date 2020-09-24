7fd6e24698b016847858859e6b005ab646ec3ce4
YuvalToker <yuval@gmail.com>
2020-09-24 13:55:12 +0300

    file from previous tries has added

---
+++ dock_snmpd/batteryAgentSubagentObject.h
@@ -0,0 +1,10 @@
+#ifndef BATTERY_AGENT_SUBAGENT_OBJECT_H
+#define BATTERY_AGENT_SUBAGENT_OBJECT_H
+
+/* function declaration */
+void init_batteryObjectAgentSubagent(void);
+Netsnmp_Node_Handler handle_batteryObject;
+void SET_batteryObject_redis();
+
+#endif /* BATTERY_AGENT_SUBAGENT_OBJECT_H */
+
