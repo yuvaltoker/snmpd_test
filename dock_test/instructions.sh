f65241ae2d399de42b3e5d404b85f467731627a7
YuvalToker <yuval@gmail.com>
2020-10-12 17:12:22 +0300

    change in instructions of dock_test

--- dock_test/instructions.sh
+++ dock_test/instructions.sh
@@ -3,9 +3,9 @@
 declare -i x=1
 sleep 15
 while true; do
-#    snmpset -v2c -c public testing_snmpd:1662 MY-SNMP-TUTORIAL-MIB::batteryObject.0 i $x >> file.log
+#    snmpset -v2c -c public testing_snmpd:1662 MY-TUTORIAL-MIB::batteryObject.0 i $x >> file.log
 #    snmpset -v2c -c public testing_snmpd:1662 NET-SNMP-TUTORIAL-MIB::nstAgentSubagentObject.0 i $x >> file.log
-#    snmpget -v2c -c public testing_snmpd:1662 MY-TUTORIAL-MIB::batterySubagentObject.0 >> file.log
+#    snmpget -v2c -c public testing_snmpd:1662 MY-TUTORIAL-MIB::batteryObject.0 >> file.log
 #    snmpget -v2c -c public testing_snmpd:1662 NET-SNMP-TUTORIAL-MIB::nstAgentSubagentObject.0 >> file.log
 
     x=x+1
